edeb09bf5eb7e8d681afa13f8c30153f1c8d746a
Youssef Amazzal <20youssefyoussef02@gmail.com>
2024-08-10 00:47:03 +0100

    add custom typography

---
+++ lib/core/configs/theme/app_typography.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTypography {
+  static const String _fontFamily = 'GreycliffCF';
+
+  static const body1 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: _fontFamily,
+    decoration: TextDecoration.none,
+  );
+
+  static const h1 = TextStyle(
+    fontSize: 96,
+    fontWeight: FontWeight.w300,
+    fontFamily: _fontFamily,
+    decoration: TextDecoration.none,
+  );
+}
